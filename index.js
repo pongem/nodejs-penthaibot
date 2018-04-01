ce55da136322322f5dae492692b1ec81d6007e5f
Pongsak Srithongnopawong <pongsaksrithongnopawong@Pongsaks-MacBook-Pro.local>
2018-04-01 23:03:50 +0700

    refactor

--- index.js
+++ index.js
@@ -1,71 +1,9 @@
-// 'use strict';
-
-// const line = require('@line/bot-sdk');
-// const express = require('express');
-
-// // create LINE SDK config from env variables
-// const config = {
-//   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-//   channelSecret: process.env.CHANNEL_SECRET,
-// };
-
-// // create LINE SDK client
-// const client = new line.Client(config);
-
-
-// // create Express app
-// // about Express itself: https://expressjs.com/
-// const app = express();
-
-// // register a webhook handler with middleware
-// // about the middleware, please refer to doc
-// app.post('/callback', line.middleware(config), (req, res) => {
-//   Promise
-//     .all(req.body.events.map(handleEvent))
-//     .then((result) => res.json(result))
-//     .catch((err) => {
-//       console.error(err);
-//       res.status(500).end();
-//     });
-// });
-
-// // event handler
-// function handleEvent(event) {
-//   if (event.type !== 'message' || event.message.type !== 'text') {
-//     // ignore non-text-message event
-//     return Promise.resolve(null);
-//   }
-
-//   // create a echoing text message
-//   const echo = { type: 'text', text: event.message.text };
-
-//   // use reply API
-//   return client.replyMessage(event.replyToken, echo);
-// }
-
-// // listen on port
-// // var https = require('https');
-// // var fs = require('fs');
-
-// // var options = {
-// //     key: fs.readFileSync('encryption/private.key'),
-// //     cert: fs.readFileSync('encryption/mydomain.crt'),
-// //     requestCert: false,
-// //     rejectUnauthorized: false
-// // };
-
-// // const server = https.createServer(options, app).listen(443, () => {
-// //   console.log('server running at ' + 443)
-// // })
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//   console.log(`listening on ${port}`);
-// });
-
 var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
 var app = express()
+
+var util = require('util');
 
 app.use(bodyParser.json())
 
@@ -81,49 +19,12 @@ app.post('/callback', (req, res) => {
   console.log(typeof sender, typeof text)
   // console.log(req.body.events[0])
   if (text === 'สวัสดี' || text === 'Hello' || text === 'hello') {
-    sendText(sender, text)
+    util.sendText(sender, text)
   }
   res.sendStatus(200)
 })
 
-function sendText (sender, text) {
-  let data = {
-    to: sender,
-    messages: [
-      {
-        type: 'text',
-        text: 'สวัสดี'
-      }
-    ]
-  }
-  request({
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${process.env.CHANNEL_ACCESS_TOKEN}`
-    },
-    url: 'https://api.line.me/v2/bot/message/push',
-    method: 'POST',
-    body: data,
-    json: true
-  }, function (err, res, body) {
-    if (err) console.log('error')
-    if (res) console.log('success')
-    if (body) console.log(body)
-  })
-}
 
 app.listen(app.get('port'), function () {
   console.log('run at port', app.get('port'))
 })
-
-// const express = require('express');
-// const app = express();
-
-// app.post('/callback', (req, res) => {
-//     res.sendStatus(200)
-// })
-
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//   console.log(`listening on ${port}`);
-// });
---
+++ util.js
@@ -0,0 +1,25 @@
+module.exports = {
+    sendText: function sendText(sender, text) {
+        let data = {
+            to: sender,
+            messages: [{
+                type: 'text',
+                text: 'สวัสดี'
+            }]
+        }
+        request({
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.CHANNEL_ACCESS_TOKEN}`
+            },
+            url: 'https://api.line.me/v2/bot/message/push',
+            method: 'POST',
+            body: data,
+            json: true
+        }, function(err, res, body) {
+            if (err) console.log('error')
+            if (res) console.log('success')
+            if (body) console.log(body)
+        })
+    }
+}
